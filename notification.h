8e84cc988db55645bcf90e4b6d14747b1b96b0f5
talhaadil48 <talhaadil48@icloud.com>
2024-11-21 17:08:39 +0000

    Update notification.h

--- notification.h
+++ notification.h
@@ -4,7 +4,6 @@
 // Function to trigger a macOS system notification
 void trigger_system_notification(const char *alert_message);
 
-// Function to send an email with the average data
-void send_email_with_daily_averages(const char *email, const char *data);
+
 
 #endif
