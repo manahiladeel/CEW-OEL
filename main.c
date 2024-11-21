6f5e9fc716e53c5ec5cadc7fb0d9643b4509c1bf
manahiladeel <manahiladeel09@gmail.com>
2024-11-21 15:23:03 +0500

    Commit

--- main.c
+++ main.c
@@ -2,15 +2,25 @@
 #include "data_handler.h"
 #include "notification.h"
 #include <string.h>
+#include <stdio.h>
+#include <time.h>
 #include "network.h"
 
 int main() {
     const char *filename = "raw_data.txt";
-    const char *date_to_find = "13-11-2024";
+
+    // Get the current date
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    // Format the date as "dd-mm-yyyy"
+    char current_date[11];
+    snprintf(current_date, sizeof(current_date), "%02d-%02d-%04d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+
+    printf("Current date: %s\n", current_date);
 
     printf("Computing daily averages...\n");
-    compute_daily_averages(filename, "processed_data.txt", date_to_find);
-    
+    compute_daily_averages(filename, "processed_data.txt", current_date);
 
     return 0;
 }
