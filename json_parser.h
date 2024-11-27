68ffe32ed2891b4eb0c8a0a9e86f0b34d2724d94
manahiladeel <manahiladeel09@gmail.com>
2024-11-27 10:27:43 +0500

    Commit

--- json_parser.h
+++ json_parser.h
@@ -1,8 +1,6 @@
 #ifndef JSON_PARSER_H
 #define JSON_PARSER_H
 
-// Function to read raw JSON data
-void read_json_data(const char *filename);
 
 // Function to compu te daily averages from raw data
 void compute_daily_averages(const char *raw_filename, const char *processed_filename, const char *date);
